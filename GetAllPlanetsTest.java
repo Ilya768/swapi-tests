6befe5c2e1a69db6b693f8fcfec60b25d61b8450
Ilya768 <49689286+Ilya768@users.noreply.github.com>
2019-04-23 17:34:41 -0400

    Update GetAllPlanetsTest.java

--- GetAllPlanetsTest.java
+++ GetAllPlanetsTest.java
@@ -3,29 +3,36 @@ package com.swapi.maven.testng;
 import org.testng.annotations.Test;
 
 public class StarWarsAPITests {
+  private static final String URL = "https://swapi.co/api/planets/";
+  private static final String firstPage = "1";
+  private static final String nextFromFirstPage = "https://swapi.co/api/planets/?page=2";
+  private static final String firstPlanetName = "Alderaan";
+  
+  private static final int allPlanets = 61;
+  private static final int resultsPerPage = 10;
+  
+  private static final String lastPage = "7";
+  private static final int resulsNumberAtLastPage = 1;
+  private static final String lastPlanetName = "Jakku";
+  private static final String previousFromLastPage = "https://swapi.co/api/planets/?page=6";
+  
+  
   @Test
   public void ResponseDataTest() {
-    given().when().get("https://swapi.co/api/planets/").
+    given().when().get(URL).
       assertThat().statusCode(200).and().
       contentType(ContentType.JSON);
   }
   
   @Test
   public void GetFirstPageResult() {
-    String pageNumber = "1";
-    String next = "https://swapi.co/api/planets/?page=2";
-    String firstPlanetName = "Alderaan";
-    
-    int allPlanets = 61;
-    int resultSize = 10;
-    
-    given().pathParam("number", pageNumber).when().
-      get("https://swapi.co/api/planets/?page={number}").
+    given().pathParam("number", firstPage).when().
+      get(URL + "?page={number}").
     then().
       assertThat().body("planets.'count'", equalTo(allPlanets)).and().
-      assertThat().body("planets.'next'", equalTo(next)).and().
+      assertThat().body("planets.'next'", equalTo(nextFromFirstPage)).and().
       assertThat().body("planets.'previous'", equalTo(null)).and().
-      assertThat().body("planets.'results.length'", equalTo(resultSize)).and().
+      assertThat().body("planets.'results.length'", equalTo(resultsPerPage)).and().
       assertThat().body("planets.'results[0].'name''", equalTo(firstPlanetName));
   }
   
@@ -41,34 +48,26 @@ public class StarWarsAPITests {
   }
   
   @Test(dataProvider = "PlanetsListResults")
-  public void GetPagesTwoToSix(String pageNumber, String previous, String next) {
-    int resultSize = 10;
-    
+  public void GetPagesTwoToSix(String pageNumber, String previous, String next) {    
     given().
       pathParam("number", pageNumber).
       pathParam("previousPage", previous).
       pathParam("nextPage", next).
-     when()get("https://swapi.co/api/planets/?page={pageNumber}").
+     when().get(URL + "?page={pageNumber}").
      then().
       assertThat().body("planets.'next'", equalTo(next)).and().
       assertThat().body("planets.'previous'", equalTo(previous)).and().
-      assertThat().body("planets.'results.length'", equalTo(resultSize));    
+      assertThat().body("planets.'results.length'", equalTo(resultsPerPage));    
   }
   
   @Test
-  public void GetLastPageResult() {
-    String pageNumber = "7";
-    String previous = "https://swapi.co/api/planets/?page=6";
-    String firstPlanetName = "Jakku";
-    
-    int resultSize = 1;
-    
-    given().pathParam("number", pageNumber).when().
-      get("https://swapi.co/api/planets/?page={number}").
+  public void GetLastPageResult() {    
+    given().pathParam("number", lastPage).when().
+      get(URL + "?page={number}").
     then().
       assertThat().body("planets.'next'", equalTo(null)).and().
-      assertThat().body("planets.'previous'", equalTo(previous)).and().
-      assertThat().body("planets.'results.length'", equalTo(resultSize)).and().
-      assertThat().body("planets.'results[0].'name''", equalTo(firstPlanetName));
+      assertThat().body("planets.'previous'", equalTo(previousFromLastPage)).and().
+      assertThat().body("planets.'results.length'", equalTo(resulsNumberAtLastPage)).and().
+      assertThat().body("planets.'results[0].'name''", equalTo(lastPlanetName));
   }
 }
