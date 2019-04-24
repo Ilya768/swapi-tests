dd59d76a49e7e5d87bf2245932be1bcd461ef018
Ilya768 <49689286+Ilya768@users.noreply.github.com>
2019-04-24 10:38:39 -0400

    Update GetAllPlanetsTest.java

--- GetAllPlanetsTest.java
+++ GetAllPlanetsTest.java
@@ -16,6 +16,37 @@ public class StarWarsAPITests {
   private static final String lastPlanetName = "Jakku";
   private static final String previousFromLastPage = "https://swapi.co/api/planets/?page=6";
   
+  /*Data set below represents the basic information about page results structure and returns a two demantional array
+  type of Object with 3 values per row in following pattern: "Page Number", "Previous Page URL", "Next Page URL". 
+  Data is hardcoded due to low number of pages. An alternative way to create such a data set is shown 
+  at "AltPlanetList" section.*/
+  @DataProvider(name="PlanetsListResult")
+  public Object[][] createTestData {
+    return new Object[][] {
+      {"2", "https://swapi.co/api/planets/?page=1", "https://swapi.co/api/planets/?page=3"},
+      {"3", "https://swapi.co/api/planets/?page=2", "https://swapi.co/api/planets/?page=4"},
+      {"4", "https://swapi.co/api/planets/?page=3", "https://swapi.co/api/planets/?page=5"},
+      {"5", "https://swapi.co/api/planets/?page=4", "https://swapi.co/api/planets/?page=6"},
+      {"6", "https://swapi.co/api/planets/?page=5", "https://swapi.co/api/planets/?page=7"},
+    };
+  }
+  
+  @DataProvider(name="AltPlanetsList")
+  public Object[][] createTestObject {
+    int last = Integer.parseInt(lastPage);
+    int dataSize = 3;
+    Object[][] dataSet = new Object[last-2][dataSize];
+    
+    for(int i = 2; i < last; i++) {
+      for(int j = 0; j < dataSize; j++) {
+        if(j == 0) dataSet[i][j] = "" + i;
+        if(j == 1) dataSet[i][j] = URL + "?page=" + (i-1);
+        if(j == 2) dataSet[i][j] = URL + "?page=" + (i+1);
+      }
+    }
+    
+    return dataSet;
+  }
   
   @Test
   public void ResponseDataTest() {
@@ -36,19 +67,8 @@ public class StarWarsAPITests {
       assertThat().body("planets.'results[0].'name''", equalTo(firstPlanetName));
   }
   
-  @DataProvider(name="PlanetsListResult")
-  public Object[][] createTestData {
-    return new Object[][] {
-      {"2", "https://swapi.co/api/planets/?page=1", "https://swapi.co/api/planets/?page=3"},
-      {"3", "https://swapi.co/api/planets/?page=2", "https://swapi.co/api/planets/?page=4"},
-      {"4", "https://swapi.co/api/planets/?page=3", "https://swapi.co/api/planets/?page=5"},
-      {"5", "https://swapi.co/api/planets/?page=4", "https://swapi.co/api/planets/?page=6"},
-      {"6", "https://swapi.co/api/planets/?page=5", "https://swapi.co/api/planets/?page=7"},
-    };
-  }
-  
   @Test(dataProvider = "PlanetsListResults")
-  public void GetPagesTwoToSix(String pageNumber, String previous, String next) {    
+  public void GetMiddlePages(String pageNumber, String previous, String next) {    
     given().
       pathParam("number", pageNumber).
       pathParam("previousPage", previous).
