d9a064b15a841b9d1fad8b85973ab93dbbb6eef6
Ilya768 <49689286+Ilya768@users.noreply.github.com>
2019-04-25 12:51:46 -0400

    Update GetAllPlanetsTest.java

--- GetAllPlanetsTest.java
+++ GetAllPlanetsTest.java
@@ -1,7 +1,7 @@
 package com.swapi.maven.testng;
 
 import org.testng.annotations.Test;
-import TestDataConstructor.TestDataManager.JsonSchemaFetcher;
+import TestDataConstructor.TestDataManager.JsonResponseProcessor;
 import TestDataConstructor.TestDataManager.MutualDataSet;
 import TestDataConstructor.TestDataManager.FirstPageDataSet;
 import TestDataConstructor.TestDataManager.LastPageDataSet;
@@ -45,6 +45,44 @@ public class StarWarsAPITests {
     return dataSet;
   }
   
+  /*----------------Test Data for Planet Tatooine, id = 1-------------*/
+  @DataProvider(name="Tatooine")
+  public HashMap<String, String> createPlanetData() {    
+    HashMap<String, String> planet = new HashMap();
+    String residents = "https://swapi.co/api/people/1/," +
+                       "https://swapi.co/api/people/2/," +
+                       "https://swapi.co/api/people/4/," +
+                       "https://swapi.co/api/people/6/," +
+                       "https://swapi.co/api/people/7/," +
+                       "https://swapi.co/api/people/8/," +
+                       "https://swapi.co/api/people/9/," +
+                       "https://swapi.co/api/people/11/," +
+                       "https://swapi.co/api/people/43/," +
+                       "https://swapi.co/api/people/62/";
+    
+    String films = "https://swapi.co/api/films/5/," +
+                   "https://swapi.co/api/films/4/," +
+                   "https://swapi.co/api/films/6/," +
+                   "https://swapi.co/api/films/3/," +
+                   "https://swapi.co/api/films/1/";
+    
+    map.put("name", "Tatooine");
+    map.put("rotation_period", "23");
+    map.put("orbital_period", "304");
+    map.put("diameter", "10465");
+    map.put("climate", "arid");
+    map.put("gravity", "1 standard");
+    map.put("terrain", "desert");
+    map.put("surface_water", "1");
+    map.put("population", "200000");
+    map.put("residents", residents);
+    map.put("films", films);
+    map.put("created", "2014-12-09T13:50:49.641000Z");
+    map.put("edited", "2014-12-21T20:48:04.175778Z");
+    map.put("url", "https://swapi.co/api/planets/1/");
+  }
+  
+  /*----------Tests for https://swapi.co/api/planets/ GET ALL-------------*/
   @Test
   public void ResponseDataTest() {
     given().when().get(URL).
@@ -97,5 +135,48 @@ public class StarWarsAPITests {
       assertThat().body("planets.'previous'", equalTo(previousFromLastPage)).and().
       assertThat().body("planets.'results.length'", equalTo(resulsNumberAtLastPage)).and().
       assertThat().body("planets.'results[0].'name''", equalTo(lastPlanetName));
+  }
+  
+  /*-------------End of Tests for https://swapi.co/api/planets/ GET ALL---------------*/
+  
+  /*-------------Tests for https://swapi.co/api/planets/{id} GET BY ID-----------------*/
+  
+  @Test
+  public void RequiredAttributesPresent() {
+    String schemaURL = "https://swapi.co/api/planets/schema";
+    
+    JsonResponseProcessor jsonRP = new JsonResponseProcessor();
+    String jsonString = jsonRP.getJsonString(schemaURL);
+    JsonObject response = jsonRP.getJsonObject(jsonString);
+    
+    String[] attributes = response.required;
+    for(int i = 1; i <= allPlanets; i++) {
+      JsonObject planetResponse = jsonRP.getJsonObject(jsonRP.getJsonString(URL + "i/"));
+      ArrayList<String> planetAttributes = jsonRP.structureJsonResponse(planetResponse);
+      assertEquals(attributes.length, planetAttributes.size());
+      
+      for(int j = 0; j < attributes.length; j++) {
+        assertEquals(attributes[j], planetAttributes.get(j));
+      }
+    }
+  }
+  
+  @Test(dataProvider = "Tatooine")
+  public void GivenIdReturnsExpectedValue(HashMap<String, String> planet) {
+    String planetURL = "https://swapi.co/api/planets/1/";
+    
+    JsonResponseProcessor jsonRP = new JsonResponseProcessor();
+    String jsonString = jsonRP.getJsonString(planetURL);
+    JsonObject response = jsonRP.getJsonObject(jsonString);
+    
+    for(String key : planet.keys()) {
+      if(response.key instanceof Array) {
+        String[] converted = planet.get(key).spli(",");
+        assertArrayEquals(response.key, planet.get(key));
+        continue;
+      }
+      
+      assertEquals(response.key, planet.get(key));
+    }
   }
 }
